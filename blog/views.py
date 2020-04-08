b907a4185bed11c8d77719918b48f31b000e14d4
Md. Omar Faruk <iamomar022@gmail.com>
2020-04-08 21:40:38 +0600

    Database and Migrations Done

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Omar Faruk',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': 'April 07, 2020'
-    },
-    {
-        'author': 'Jannat Akter',
-        'title': 'Blog Post 2',
-        'content': 'Second Post Content',
-        'date_posted': 'April 08, 2020'
-    }
-]
-
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 

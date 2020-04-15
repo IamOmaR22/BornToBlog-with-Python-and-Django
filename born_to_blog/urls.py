bdc9d3f84f01d073bcf9b4180b7eae4703e75963
Md. Omar Faruk <iamomar022@gmail.com>
2020-04-15 08:45:47 +0600

    Login Routes and Templates

--- born_to_blog/urls.py
+++ born_to_blog/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),       # Class Base View
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),    # Class Base View
 ]

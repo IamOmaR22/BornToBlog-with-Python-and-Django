cf4f8a68d1de5983b33d0d16be6358afcdd11b9e
Md. Omar Faruk <iamomar022@gmail.com>
2020-04-16 11:14:34 +0600

    User Profile and Default Image

--- born_to_blog/urls.py
+++ born_to_blog/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,3 +13,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),    # Class Base View
     path('profile/', user_views.profile, name='profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

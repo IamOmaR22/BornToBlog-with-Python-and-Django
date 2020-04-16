616fe115ca69012c8b87a9846be123694620d5d7
Md. Omar Faruk <iamomar022@gmail.com>
2020-04-16 09:54:35 +0600

    Profile Model And Register

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)   # One-To-One Relationship With The Existing User Model
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')   # upload_to means directory, which will create to store profile images.
+
+
+    def __str__(self):
+        return f'{self.user.username} Profile'   # It's Called f'{}' string

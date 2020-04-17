82fb59219a9f9f7722ea96677f07b576cb6f6029
Md. Omar Faruk <iamomar022@gmail.com>
2020-04-17 10:28:18 +0600

    Resize Image After upload

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image  # Import Image From Pillow Library. To Grab The Image and Resize It.
 
 # Create your models here.
 class Profile(models.Model):
@@ -9,3 +10,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'   # It's Called f'{}' string
+
+ # Resize Image Automatically
+    def save(self):
+        super().save()  # Run The Save Method Of Our Parent Class.
+
+        img = Image.open(self.image.path)  # self.image.path will open image of current instance.
+
+        if img.height > 300 or img.width > 300:  # Resize The Image.
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path) # overwrite the large image
--- users/views.py
+++ users/views.py
@@ -26,8 +26,19 @@ def register(request):
 
 @login_required
 def profile(request):
-    u_form = UserUpdateForm()
-    p_form = ProfileUpdateForm()
+    if request.method == 'POST':   # This Will Be Run When I Submit My Form. And Possibly Pass New Data.
+        u_form = UserUpdateForm(request.POST, instance=request.user)  # request.POST To Pass The POST Data
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)  # File Data (images) Users Try To Upload.
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your profile has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {   # To Pass This Into Template We Used context.
         'u_form': u_form,

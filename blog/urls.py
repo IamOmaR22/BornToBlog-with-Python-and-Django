09fd72d34c7d82bd2a654696edaab83729f14980
Md. Omar Faruk <iamomar022@gmail.com>
2020-04-18 09:28:51 +0600

    PostDetailView and PostListView

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    # path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # post/1 or post/2 ... etc
     path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+# def home(request):
+#     context = {
+#         'posts': Post.objects.all()
+#     }
+#     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post  # This will tell my ListView. What model to query and In order to create the list.
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']  # ['date_posted'] - oldest to newest and ['-date_posted'] - newest to oldest
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'  # <app>/<model>_<viewtype>.html
+
+
 
 
 def about(request):

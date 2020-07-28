8732709183e0fce03e91219fbb3b720803cb2a59
Md. Omar Faruk <iamomar022@gmail.com>
2020-07-28 07:48:53 +0600

    Security

--- born_to_blog/settings.py
+++ born_to_blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&gl!z+310@db4p2jza*5_r98-==9ok%(5xk#z%8ut9aya7x8r+'
+SECRET_KEY = '&gomarl!z+310@db4p2jza*5_r98-==9ok%(5xk#zfaruk%8ut9aya7x8r+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

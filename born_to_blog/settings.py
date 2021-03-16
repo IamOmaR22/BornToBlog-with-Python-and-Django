3cc2efa86ced01e6a0dd625c2e81d6e869fb6e49
Md. Omar Faruk <iamomar022@gmail.com>
2021-03-16 16:34:47 +0600

    static root

--- born_to_blog/settings.py
+++ born_to_blog/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
